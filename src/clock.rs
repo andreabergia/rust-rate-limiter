a74a38b08afe028bcea8f6a5fc32afb2f23d6c88
Andrea Bergia <andrea.bergia@lastminute.com>
2022-11-22 16:48:31 +0100

    feat: real clock

--- src/clock.rs
+++ src/clock.rs
@@ -1,3 +1,5 @@
+use time::{Duration, OffsetDateTime};
+
 type UnixTimestamp = i64;
 
 pub trait Clock {
@@ -11,5 +13,27 @@ pub struct FixedClock {
 impl Clock for FixedClock {
     fn current_timestamp(&self) -> UnixTimestamp {
         self.value
+    }
+}
+
+pub struct MillisecondsUnixClock {}
+
+impl Clock for MillisecondsUnixClock {
+    fn current_timestamp(&self) -> UnixTimestamp {
+        let nanos = OffsetDateTime::now_utc().unix_timestamp_nanos();
+        let millis = nanos / 1_000_000;
+        return millis as UnixTimestamp;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Clock, MillisecondsUnixClock};
+
+    #[test]
+    fn unix_clock_works() {
+        let clock = MillisecondsUnixClock {};
+        // Approximate timestamp at the time of writing this code
+        assert!(clock.current_timestamp() > 1_669_132_053_000);
     }
 }
