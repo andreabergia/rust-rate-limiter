d58d62e9213cc3137de6a2489a5064c73eadf881
Andrea Bergia <andrea.bergia@lastminute.com>
2022-11-22 16:53:11 +0100

    feat: added tiny http server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
 mod clock;
 mod rate_limiter;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(say_hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn say_hello() -> &'static str {
+    "Hello, World!"
 }
