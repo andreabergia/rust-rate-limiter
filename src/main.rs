34208757b6842f54a73e25c7638ca4de9b355ee7
Andrea Bergia <andrea.bergia@lastminute.com>
2022-11-23 08:39:48 +0100

    feat: added rate limiter to the hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension, Router};
+use clock::MillisecondsUnixClock;
+use rate_limiter::{RateLimiter, RequestKey, RequestProcessingResponse, Result};
 
 mod clock;
 mod rate_limiter;
+
+type RateLimiterOfMsUnixClock = RateLimiter<MillisecondsUnixClock>;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(say_hello));
+    let clock = Arc::new(Mutex::new(MillisecondsUnixClock {}));
+    let rate_limiter = RateLimiter::new(clock, 1, 2_000);
+    let rate_limiter = Arc::new(Mutex::new(rate_limiter));
+
+    let app = Router::new()
+        .route("/", get(say_hello))
+        .layer(Extension(rate_limiter));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
     tracing::debug!("listening on {}", addr);
@@ -19,6 +32,13 @@ async fn main() {
         .unwrap();
 }
 
-async fn say_hello() -> &'static str {
-    "Hello, World!"
+async fn say_hello(
+    Extension(rate_limiter): Extension<Arc<Mutex<RateLimiterOfMsUnixClock>>>,
+) -> Result<impl IntoResponse> {
+    let address = RequestKey::new("todo");
+    let result = rate_limiter.lock()?.try_add_request(address)?;
+    match result {
+        RequestProcessingResponse::Allow => Ok(StatusCode::NO_CONTENT),
+        RequestProcessingResponse::Deny => Ok(StatusCode::TOO_MANY_REQUESTS),
+    }
 }
