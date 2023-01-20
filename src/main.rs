9482db980eec80be5afdad2bf14f58ce518888e9
Andrea Bergia <andrea.bergia@lastminute.com>
2023-01-20 11:40:42 +0100

    feat: added request source address

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension, Router};
+use axum::{
+    extract::ConnectInfo, http::StatusCode, response::IntoResponse, routing::get, Extension, Router,
+};
 use clock::UnixEpochMillisecondsClock;
 use error::Result;
 use rate_limiter::{RateLimiter, RequestKey, RequestProcessingResponse};
+use tracing::info;
 
 mod clock;
 mod error;
@@ -29,17 +32,18 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
 }
 
 async fn say_hello_rate_limited(
     Extension(rate_limiter): Extension<Arc<Mutex<RateLimiterOfUnixEpochMsClock>>>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
 ) -> Result<impl IntoResponse> {
-    // TODO: extract source address
-    let address = RequestKey::new("todo");
+    let address = RequestKey::new(&format!("{}", addr.ip()));
     let result = rate_limiter.lock()?.try_add_request(address)?;
+    info!("request from client {}: {:?}", addr, result);
     match result {
         RequestProcessingResponse::Allow => Ok((StatusCode::OK, "Hello!").into_response()),
         RequestProcessingResponse::Deny => Ok(StatusCode::TOO_MANY_REQUESTS.into_response()),
